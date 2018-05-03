28920ba95c06f799d8801b4c7415132a859214f8
raulubaldi01 <raulubaldi01@outlook.it>
2018-05-03 17:37:16 +0200

    Verifica

---
+++ Ubaldi_Finale/StatisticaLibrary.Test/StatisticaTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StatisticaLibrary;
+
+namespace StatisticaLibrary.Test
+{
+    [TestClass]
+    public class StatisticaTest
+    {
+        [TestMethod]
+        public void TestPositivo()
+        {
+            double a = 3;
+            bool ris = true;
+
+            bool resp = Statistica.IsPositivo(a, ris);
+            Assert.AreEqual(ris, resp);
+        }
+        [TestMethod]
+        public void TestNegativo()
+        {
+            double a = -3;
+            bool ris = false;
+
+            bool resp = Statistica.IsPositivo(a, ris);
+            Assert.AreEqual(ris, resp);
+        }
+        [TestMethod]
+        public void TestNeutro()
+        {
+            double a = 0;
+            bool ris = false;
+
+            bool resp = Statistica.IsPositivo(a, ris);
+            Assert.AreEqual(ris, resp);
+        }
+
+        [TestMethod]
+        public void TestCaricaPositiva()
+        {
+            int[] a = new int []{ 1, -3 , 5 , -6 };
+            int ctr = 2;
+
+            int resp = Statistica.CaricaPositiva(a, ctr);
+            Assert.AreEqual(resp, ctr);
+        }
+        [TestMethod]
+        public void TestPositivi()
+        {
+            int[] a = new int[] { 1, -3, 5, -6, 0 };
+            int[] b = new int[] { 1,5 };
+             
+
+            int[] resp = Statistica.Positivi(a, b);
+            CollectionAssert.AreEqual(b, resp);
+        }
+    }
+}
---
+++ Ubaldi_Finale/StatisticaLibrary/Statistica.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatisticaLibrary
+{
+    public class Statistica
+    {
+        public static bool IsPositivo (double a,bool ris)
+        {
+            ris = false;
+            if (a > 0)
+            {
+                ris = true;
+            }
+            return ris;
+        }
+        public static int CaricaPositiva(int[]a,int ctr)
+        {
+            for(int i = 0; i < a.Length; i++)
+            {
+                if(a[i]>0)
+                {
+                    ctr = ctr + 1;
+                }
+            }
+            return ctr; 
+        }
+        public static int[] Positivi(int[] a, int[] b)
+        {
+            for(int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > 0)
+                {
+                    b[i] = a[i];
+                }
+            }
+
+            return b;
+        }
+    }
+}
